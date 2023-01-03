b2918787239941dd5f142f1af6bf853f414fa49c
Arijit Basu <sayanarijit@gmail.com>
2023-01-03 13:57:28 +0530

    Server side

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+}
+
+const HTML = `
+<html>
+  <body>
+    <p>Let me present your server side cookies...</p>
+    <pre>{{sscookies}}</pre>
+  </body>
+</html>
+`;
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    let html = HTML.replace(
+      "{{sscookies}}",
+      (request.headers.get("Cookie") || "").toString().replaceAll("; ", "\n")
+    );
+
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    });
+  },
+};
